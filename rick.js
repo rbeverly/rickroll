51bc77ce911cc4dc417015c62e1fd00f83797855
Rab Beverly <rab@robertbeverly.com>
2022-07-28 01:44:08 -0400

    oh just some comments n stuff... nothing to see here.

--- rick.js
+++ rick.js
@@ -15,11 +15,13 @@
     }
 })();
 
+// Blessed are the F12ers, for they shall inherit the ctf.
 (async function() {
-    var song = ["Never gonna give you up,", "Never gonna let you down,", "Never gonna run around", "And desert you <3", "Never gonna make you cry,", "Never gonna say goodbye,", "Never gonna tell a lie", "An hurt you :`("];
+    var secretSquirrel = btoa('66 6c 61 67 7b 79 6f 75 2d 67 30 74 2d 72 31 63 6b 72 30 6c 6c 33 64 7d')
+    var song = ["Never gonna give you up,", "Never gonna let you down,", "Never gonna run around", "And desert you <3", "Never gonna make you cry,", "Never gonna say goodbye,", "Never gonna tell a lie", "An hurt you :`(", secretSquirrel];
     var n = -1;
     while (true) {
-        (n > 6) ? n = 0 : n++;
+        (n > 7) ? n = 0 : n++;
         console.log(song[n]);
         await new Promise(r => setTimeout(r, 1337));
         // A promise... to never say goodbye, or tell a lie and hurt you.
